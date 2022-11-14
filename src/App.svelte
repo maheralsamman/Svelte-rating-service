9f7c6d949950b784f389483e4ab3c29c4be9df8b
maheralsamman <supermaher2@gmail.com>
2022-11-14 18:47:06 +0100

    first

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+    import FeedbackList from "./components/FeedbackList.svelte"
+    import FeedbackStats from "./components/FeedbackStats.svelte"
+    import FeedbackForm from "./components/FeedbackForm.svelte"
+
+ let feedback = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+]
+
+$: count = feedback.length;
+$: average = feedback.reduce((a, {rating}) => a + rating , 0) / count;
+const deleteFeedback = (e) => {
+    const itemId = e.detail;
+    feedback = feedback.filter(item => item.id !== itemId)
+}
+  </script>
+  
+  <main class="container">
+    <FeedbackForm />
+    <FeedbackStats {count} {average}/>
+    <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+
+  </main>
+  
+  <style>
+
+  </style>
+  
