0d44634c1265e6a77a8ca05881d05718ff01273f
maheralsamman <supermaher2@gmail.com>
2022-11-14 19:29:04 +0100

    update store

--- src/stores.js
+++ src/stores.js
@@ -6,9 +6,4 @@ export const FeedbackStore = writable([
     rating: 10,
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
   },
-  {
-    id: 2,
-    rating: 9,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-  },
 ]);
