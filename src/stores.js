a8be4f6b33d6eb940ee044b1f9af6d10a21a8399
maheralsamman <supermaher2@gmail.com>
2022-11-14 19:29:31 +0100

    update store

--- src/stores.js
+++ src/stores.js
@@ -1,5 +1,14 @@
 import { writable } from "svelte/store";
 
 export const FeedbackStore = writable([
-
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
 ]);
